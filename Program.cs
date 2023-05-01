b327b00a198463563862049c561c285f6ecdebb2
Nikhitha <nikitha.kunapareddy@sailssoftware.com>
2023-05-01 12:32:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Singleton_Design_pattern;
+using System;
+namespace Singleton_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromTeachaer = Singleton.GetInstance();
+            fromTeachaer.PrintDetails("From Teacher");
+
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromStudent = Singleton.GetInstance();
+            fromStudent.PrintDetails("From Student");
+
+            Console.ReadLine();
+        }
+    }
+}
